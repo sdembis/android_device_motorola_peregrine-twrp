86fa4ca268e052e2e9992dcb0c0a5e446f0f28e8
LuK1337 <priv.luk@gmail.com>
2016-11-14 21:29:14 +0100

    peregrine: Add androidboot.bootdevice=msm_sdcc.1 to cmdline
    Change-Id: I18e1db8cee28a00d5fcc8b8b7807e6f49aa1b593

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 # Kernel Prebuilt
 TARGET_PREBUILT_KERNEL := device/motorola/peregrine/kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/motorola/peregrine/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom vmalloc=400M androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.bootdevice=msm_sdcc.1 androidboot.hardware=qcom vmalloc=400M androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/motorola/peregrine/dt.img
 
