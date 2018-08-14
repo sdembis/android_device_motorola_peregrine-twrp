572acd36c3b6cd3c8756ea2b6f2f66271be2cc9c
ThePiGuy0 <thepiguy0@gmail.com>
2018-08-14 09:58:20 +0100

    Move to source built kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,12 +15,12 @@ TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := krait
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-# Kernel Prebuilt
-TARGET_PREBUILT_KERNEL := device/motorola/peregrine/kernel
-BOARD_CUSTOM_BOOTIMG_MK := device/motorola/peregrine/mkbootimg.mk
+# Kernel
+TARGET_KERNEL_CONFIG := peregrine_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/msm8226
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_CMDLINE := androidboot.bootdevice=msm_sdcc.1 androidboot.hardware=qcom vmalloc=400M androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/motorola/peregrine/dt.img
 
 # Init
 TARGET_INCREASES_COLDBOOT_TIMEOUT := true
