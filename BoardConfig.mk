ffb9d7c09e295258d3ed95e071e94de372d2c839
sdembis <sdembiske@gmail.com>
2020-02-23 10:44:04 -0800

    Changes to be committed: 	modified:   BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,12 +15,12 @@ TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := krait
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-# Kernel
-TARGET_KERNEL_CONFIG := peregrine_defconfig
-TARGET_KERNEL_SOURCE := kernel/motorola/msm8226
-BOARD_KERNEL_IMAGE_NAME := zImage
+# Kernel Prebuilt
+TARGET_PREBUILT_KERNEL := device/motorola/peregrine/kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/motorola/peregrine/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := androidboot.bootdevice=msm_sdcc.1 androidboot.hardware=qcom vmalloc=400M androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/motorola/peregrine/dt.img
 
 # Init
 TARGET_INCREASES_COLDBOOT_TIMEOUT := true
@@ -46,6 +46,5 @@ TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_NTFS_3G := true
 TW_NO_USB_STORAGE := true
 TW_THEME := portrait_hdpi
-TW_EXCLUDE_MTP := true
 
 
